23f438cedd68c0c3c5b6f6b8d11bc33995c497a3
mdfayaz2 <kmdfayaz@lyrostech.com>
2024-03-10 11:52:55 +0530

    final

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
- function Header() {
-  return (
-    <>
-    <div>This is Header</div>
-    </>
-  )
-}
+const Header = () => { const myStyle = { color: "red",
+backgroundColor: "Blue", padding: "12px", fontFamily: "Sans-Serif"
+};
+return (
+<>
+<h1 style={myStyle}>Hello World!</h1>
+<p>Heading</p>
+</>
+); }
+//  function Header() {
+//   return (
+//     <>
+//     <div></div>
+//     </>
+//   )
+// }
 export default Header;
