e9727847725e932e9e71682e404acff69f614d68
mdfayaz2 <kmdfayaz@lyrostech.com>
2024-03-18 16:20:05 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App25 from "./App25";
+import App29 from "./App29";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App25 />
+    <App29 />
   </>
 );
